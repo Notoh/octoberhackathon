f78168227567ff339c95da5b0856aae0eb4019e9
Forsight16 <Nikoleeforsyth@gmail.com>
2020-10-11 13:51:14 -0600

    1:50 )
    q!

---
+++ Assets/Scripts/AudioEffects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEffects : MonoBehaviour
+{
+    public AudioSource Music;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Music.pitch <= 1)
+        {
+            Music.pitch += 0.01f;
+        }
+    }
+}
---
+++ Assets/Scripts/Slashing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slashing : MonoBehaviour
+{
+    [SerializeField] private Transform[] points;
+    [SerializeField] private Transform[] blocks;
+    public Transform Sword;
+    public int index;
+    public int index2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (index == 0)
+        {
+            Sword.position = blocks[index2].position;
+            Sword.rotation = blocks[index2].rotation;
+        }
+        else
+        {
+        Sword.position = points[index].position;
+        Sword.rotation = points[index].rotation;
+        }
+        
+    }
+}
